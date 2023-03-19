73475c08da7e158c6df9259aed4977325d906be6
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-03-19 17:48:53 +0100

    Add project files.

---
+++ Projekt.intranet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projekt.intranet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Projekt.portalWWW/wwwroot/js/site.js
@@ -0,0 +1,2 @@
+﻿
+$('.dropdown-trigger').dropdown();
