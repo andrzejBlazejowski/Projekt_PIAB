eb8fab010e84e69817bc9bcebd7f6829b4194349
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-05-26 21:23:39 +0200

    added product details, added filtering and sorting

--- Projekt.portalWWW/Controllers/BaseController.cs
+++ Projekt.portalWWW/Controllers/BaseController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Projekt.Data.Data;
 using Projekt.Data.Data.CMS;
+using Projekt.Data.Data.Shop;
 using Projekt.portalWWW.Models;
 using System.Diagnostics;
 
@@ -21,11 +22,19 @@ namespace Projekt.portalWWW.Controllers
             ViewBag.Sites =
                 (
                     from Site in _context.Site
+                    where Site.IsActive
                     select Site
                 ).ToList();
 
+            ViewBag.ProductCategories =
+                (
+                    from ProductCategory in _context.ProductCategory
+                    where ProductCategory.IsActive
+                    select ProductCategory
+                   
+                ).ToList();
 
-            ViewBag.SiteParameters = _context.Parameter
+            ViewBag.SiteParameters = _context.Parameter.Where(p => p.IsActive == true)
                 .Select(p => new { p.Key, p.Value })
                 .ToDictionary(p => p.Key, p => p.Value);
             /*
--- Projekt.portalWWW/Controllers/ShopController.cs
+++ Projekt.portalWWW/Controllers/ShopController.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Projekt.Data.Data;
-using Projekt.Data.Data.CMS;
 using Projekt.Data.Data.Sharded;
-using Projekt.portalWWW.Models;
-using System.Diagnostics;
+using Projekt.Data.Data.Shop;
 
 namespace Projekt.portalWWW.Controllers
 {
@@ -12,19 +10,90 @@ namespace Projekt.portalWWW.Controllers
     {
         public ShopController(ProjectContext context) : base(context) { }
 
-        public async override Task<IActionResult> Index(int id)
+        [HttpGet("Index/{categoryId}")]
+        public async override Task<IActionResult> Index(int categoryId)
         {
             prepareLayoutData();
 
             var products = (
                 from Product in _context.Product
+                where Product.IsActive && Product.IsVisible && 
+                    (Product.ProductCategoryId == categoryId || categoryId == 0) 
                 select Product
-            ).ToList();
+            ).OrderBy(p => p.CreationDate).ToList();
 
             foreach (var product in products)
             {
                 Picture HeaderImage = await _context.Picture.FirstOrDefaultAsync(p => p.Id == product.ImageId);
                 product.Image = HeaderImage;
+            }
+
+            return View(products);
+        }
+
+        [HttpGet("Index/{categoryId}/{sortOrder?}/{minimumPrice?}/{maximumPrice?}")]
+        public async Task<IActionResult> Index(int categoryId, string sortOrder, decimal? minimumPrice, decimal? maximumPrice)
+        {
+            prepareLayoutData();
+
+            var productsQuery = _context.Product
+                .Where(p => p.IsActive && p.IsVisible && (p.ProductCategoryId == categoryId || categoryId == 0));
+
+            if (minimumPrice.HasValue)
+            {
+                productsQuery = productsQuery.Where(p => p.Price >= minimumPrice.Value);
+            }
+
+            if (maximumPrice.HasValue)
+            {
+                productsQuery = productsQuery.Where(p => p.Price <= maximumPrice.Value);
+            }
+
+            switch (sortOrder)
+            {
+                case "Name":
+                    productsQuery = productsQuery.OrderBy(p => p.Name);
+                    break;
+                case "Price":
+                    productsQuery = productsQuery.OrderBy(p => p.Price);
+                    break;
+                case "dateAdded":
+                default:
+                    productsQuery = productsQuery.OrderBy(p => p.CreationDate);
+                    break;
+            }
+
+            var products = await productsQuery.ToListAsync();
+
+            foreach (var product in products)
+            {
+                Picture headerImage = await _context.Picture.FirstOrDefaultAsync(p => p.Id == product.ImageId);
+                product.Image = headerImage;
+            }
+
+            return View(products);
+        }
+
+
+        [HttpGet("Index/{categoryId}/{searchString}")]
+        public async Task<IActionResult> Index(int categoryId, string searchString)
+        {
+            prepareLayoutData();
+
+            var productsQuery = _context.Product
+                .Where(p => p.IsActive && p.IsVisible && (p.ProductCategoryId == categoryId || categoryId == 0));
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                productsQuery = productsQuery.Where(p => p.Name.Contains(searchString) || p.Description.Contains(searchString) || p.Content.Contains(searchString));
+            }
+
+            var products = await productsQuery.ToListAsync();
+
+            foreach (var product in products)
+            {
+                Picture headerImage = await _context.Picture.FirstOrDefaultAsync(p => p.Id == product.ImageId);
+                product.Image = headerImage;
             }
 
             return View(products);
